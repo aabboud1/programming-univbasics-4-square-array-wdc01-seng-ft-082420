2c4fe71863c1df0ae50c70c6808cd221699cbd2f
immutable-hashmap-7791 <abboudahmed1@gmail.com>
2020-08-09 19:27:13 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,9 @@
 def square_array(array)
   # your code here
+  length = array.length
+  new_array = []
+  length.times do |array|
+    new_array.push(array|array| * array|array|)
+  end
+  new_array
 end
