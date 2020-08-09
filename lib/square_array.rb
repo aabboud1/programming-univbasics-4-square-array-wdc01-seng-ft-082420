a4f323a3f5f8429b7d55a34e1763690042026edf
immutable-hashmap-7791 <abboudahmed1@gmail.com>
2020-08-09 19:32:03 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,11 @@ def square_array(array)
   # your code here
  results = []
  counter = 0 
- while counter < array.counter do
-   
+ while counter < array.length do
+   current_item = array[counter]
+   results << current_item ** 2 
+   counter += 1 
+ end
+ results
  
 end
